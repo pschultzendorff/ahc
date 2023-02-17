8a552dbd3d48d65a6f86c1c9665b1dbf54c766b9
pschultzendorff <peter@schultzendorff.de>
2023-02-17 11:36:58 +0100

    ENH: Add rel_perm np functions

--- src/tpf_lab/ml/rel_perm.py
+++ src/tpf_lab/ml/rel_perm.py
@@ -1,5 +1,6 @@
-import torch.nn as nn
+import numpy as np
 import torch
+import torch.nn as nn
 
 
 class RelPermNN(nn.Module):
@@ -18,3 +19,33 @@ class RelPermNN(nn.Module):
         x = self.act1(self.fc2(x))
         x = self.act1(self.fc3(x))
         return self.act2(self.fc4(x))
+
+
+def error_func_deriv(
+    x: np.ndarray, yscale: float = 1.0, xscale: float = 200, offset: float = 0.5
+) -> np.ndarray:
+    return yscale * np.exp(-xscale * (x - offset) ** 2)
+
+
+def brookscorey_w(
+    s: np.ndarray,
+    n1: float = 2.0,
+    n2: float = 3.0,
+    n3: float = 1.0,
+    residual_sat_w: float = 0.1,
+    residual_sat_n: float = 0.0,
+) -> np.ndarray:
+    normalized_s = (s - residual_sat_w) / (1 - residual_sat_n - residual_sat_w)
+    return normalized_s ** (n1 + n2 * n3)
+
+
+def brookscorey_n(
+    s: np.ndarray,
+    n1: float = 2.0,
+    n2: float = 3.0,
+    n3: float = 1.0,
+    residual_sat_w: float = 0.1,
+    residual_sat_n: float = 0.0,
+) -> np.ndarray:
+    normalized_s = (s - residual_sat_w) / (1 - residual_sat_n - residual_sat_w)
+    return normalized_s ** (n1 + n2 * n3)
