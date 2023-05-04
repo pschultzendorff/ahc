MAINT: Small changes to bc. Add example script for wrong scaling.

src/tpf_lab/models/buckley_leverett.py
@@ -118,8 +118,8 @@ class BuckleyLeverett(TwoPhaseFlow):
         # Set the wetting mobility at the boundaries to the wetting mobility at residual
         # saturations.
         # For PorePy reasons the values needs to be negative.
-        array[-1] = -((1 - 0.5) ** 3) * self._rel_perm_linear_param
-        array[0] = 0.01
+        array[-1] = -0.99
+        array[0] = -0.01
         return array
 
     def _bc_values_mobility_n(self, g: pp.Grid) -> np.ndarray:
@@ -127,8 +127,8 @@ class BuckleyLeverett(TwoPhaseFlow):
         # Set the nonwetting mobility at the boundaries to the nonwetting mobility at
         # residual saturations.
         # For PorePy reasons the values needs to be negative.
-        array[-1] = 0.01
-        array[0] = -((1 - 0.5) ** 3) * self._rel_perm_linear_param
+        array[-1] = -0.01
+        array[0] = -0.99
         return array
 
     def _initial_condition(self) -> None:

tests/integration/test_two_phase_flow.py
@@ -157,8 +157,7 @@ def s_normalized_jac(saturation_w_init, model: TwoPhaseFlow_with_source) -> np.n
 
 
 def test_normalized_s(
-    model: TwoPhaseFlow_with_source,
-    s_normalized_jac: np.ndarray,
+    model: TwoPhaseFlow_with_source, s_normalized_jac: np.ndarray
 ) -> None:
     """The model uses the normalized saturation for the rel. perm. and cap. pressure
     functions. We check that its Jacobian is calculated correctly."""